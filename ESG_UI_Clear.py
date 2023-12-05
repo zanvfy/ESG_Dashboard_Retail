cadd5a245bcbb89a0125f8fe48bf6607d095057c
Manisha <mannu.r.rao@gmail.com>
2023-12-04 22:57:43 -0500

    somechanges in insert statement
    Signed-off-by: Manisha <mannu.r.rao@gmail.com>

--- ESG_UI_Clear.py
+++ ESG_UI_Clear.py
@@ -4,8 +4,8 @@ import tkinter as tk
 # Function to clear Waste Management entries after submission
 def clear_waste_entries():
     EUI.store_id_waste_entry.delete(0, tk.END)
-    EUI.date_generated_entry.delete(0, tk.END)
-    EUI.date_entered_entry.delete(0, tk.END)
+    EUI.date_generated_energy_entry.delete(0, tk.END)
+    # EUI.date_entered_energy_entry.delete(0, tk.END)
     EUI.data_load_type_entry.delete(0, tk.END)
     EUI.food_wastage_entry.delete(0, tk.END)
     EUI.food_wastage_units_entry.delete(0, tk.END)
@@ -18,7 +18,7 @@ def clear_waste_entries():
 def clear_energy_entries():
     EUI.store_id_energy_entry.delete(0, tk.END)
     EUI.date_generated_energy_entry.delete(0, tk.END)
-    EUI.date_entered_energy_entry.delete(0, tk.END)
+    # EUI.date_entered_energy_entry.delete(0, tk.END)
     EUI.data_load_type_energy_entry.delete(0, tk.END)
     EUI.light_consumption_entry.delete(0, tk.END)
     EUI.light_consumption_units_entry.delete(0, tk.END)
@@ -33,7 +33,7 @@ def clear_energy_entries():
 def clear_water_entries():
     EUI.store_id_water_entry.delete(0, tk.END)
     EUI.date_generated_water_entry.delete(0, tk.END)
-    EUI.date_entered_water_entry.delete(0, tk.END)
+    # EUI.date_entered_water_entry.delete(0, tk.END)
     EUI.data_load_type_water_entry.delete(0, tk.END)
     EUI.drinking_water_consumption_entry.delete(0, tk.END)
     EUI.drinking_water_consumption_units_entry.delete(0, tk.END)
@@ -46,7 +46,7 @@ def clear_water_entries():
 def clear_fuel_entries():
     EUI.store_id_fuel_entry.delete(0, tk.END)
     EUI.date_generated_fuel_entry.delete(0, tk.END)
-    EUI.date_entered_fuel_entry.delete(0, tk.END)
+    # EUI.date_entered_fuel_entry.delete(0, tk.END)
     EUI.data_load_type_fuel_entry.delete(0, tk.END)
     EUI.gasoline_consumed_entry.delete(0, tk.END)
     EUI.gasoline_consumed_units_entry.delete(0, tk.END)

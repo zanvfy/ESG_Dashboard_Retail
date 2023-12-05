cea1340da0b697f6e785e3d5cfdc0ac832e49783
Inder <indersingh57922@gmail.com>
2023-12-04 16:14:34 -0800

    database files
    Signed-off-by: Inder <indersingh57922@gmail.com>

---
+++ Database/Insert_Fuel_Consumption.sql
@@ -0,0 +1,51 @@
+INSERT INTO public.fuelconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, gasoline_consumed, gasoline_consumed_units, diesel_consumed, diesel_consumed_units, electric_consumed, electric_consumed_units)
+	VALUES (1, '2023-08-31', '2023-09-12', 'Monthly',1187, 'Kg', 768,'Litre' ,247, 'kWh');
+INSERT INTO public.fuelconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, gasoline_consumed, gasoline_consumed_units, diesel_consumed, diesel_consumed_units, electric_consumed, electric_consumed_units)
+	VALUES (1, '2023-09-30',' 2023-10-11', 'Monthly', 1298,'kg', 810, 'Litre', 234,'kWh' );
+INSERT INTO public.fuelconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, gasoline_consumed, gasoline_consumed_units, diesel_consumed, diesel_consumed_units, electric_consumed, electric_consumed_units)
+	VALUES (1, '2023-10-31', '2023-11-04', 'Monthly', 1341, 'Kg', 671,'Litre' , 220,'kWh' );
+
+INSERT INTO public.fuelconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, gasoline_consumed, gasoline_consumed_units, diesel_consumed, diesel_consumed_units, electric_consumed, electric_consumed_units)
+	VALUES (2, '2023-08-31', '2023-09-12', 'Monthly',1157, 'Kg', 668,'Litre' ,323, 'kWh');
+INSERT INTO public.fuelconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, gasoline_consumed, gasoline_consumed_units, diesel_consumed, diesel_consumed_units, electric_consumed, electric_consumed_units)
+	VALUES (2, '2023-09-30',' 2023-10-11', 'Monthly', 1196,'kg', 710, 'Litre', 312,'kWh' );
+INSERT INTO public.fuelconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, gasoline_consumed, gasoline_consumed_units, diesel_consumed, diesel_consumed_units, electric_consumed, electric_consumed_units)
+	VALUES (2, '2023-10-31', '2023-11-04', 'Monthly', 1249, 'Kg', 779,'Litre' , 369,'kWh' );
+
+INSERT INTO public.fuelconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, gasoline_consumed, gasoline_consumed_units, diesel_consumed, diesel_consumed_units, electric_consumed, electric_consumed_units)
+	VALUES (3, '2023-08-31', '2023-09-12', 'Monthly',1548, 'Kg', 996,'Litre' ,523, 'kWh');
+INSERT INTO public.fuelconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, gasoline_consumed, gasoline_consumed_units, diesel_consumed, diesel_consumed_units, electric_consumed, electric_consumed_units)
+	VALUES (3, '2023-09-30',' 2023-10-11', 'Monthly', 1675,'kg', 1013, 'Litre', 567,'kWh' );
+INSERT INTO public.fuelconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, gasoline_consumed, gasoline_consumed_units, diesel_consumed, diesel_consumed_units, electric_consumed, electric_consumed_units)
+	VALUES (3, '2023-10-31', '2023-11-04', 'Monthly', 1725, 'Kg', 1178,'Litre' , 615,'kWh' );
+
+INSERT INTO public.fuelconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, gasoline_consumed, gasoline_consumed_units, diesel_consumed, diesel_consumed_units, electric_consumed, electric_consumed_units)
+	VALUES (4, '2023-08-31', '2023-09-05', 'Monthly',1285, 'Kg', 568,'Litre' ,179, 'kWh');
+INSERT INTO public.fuelconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, gasoline_consumed, gasoline_consumed_units, diesel_consumed, diesel_consumed_units, electric_consumed, electric_consumed_units)
+	VALUES (4, '2023-09-30',' 2023-10-18', 'Monthly', 1234,'kg', 610, 'Litre', 170,'kWh' );
+INSERT INTO public.fuelconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, gasoline_consumed, gasoline_consumed_units, diesel_consumed, diesel_consumed_units, electric_consumed, electric_consumed_units)
+	VALUES (4, '2023-10-31', '2023-11-09', 'Monthly', 1223, 'Kg', 679,'Litre' , 186,'kWh' );
+
+INSERT INTO public.fuelconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, gasoline_consumed, gasoline_consumed_units, diesel_consumed, diesel_consumed_units, electric_consumed, electric_consumed_units)
+	VALUES (5, '2023-08-31', '2023-09-16', 'Monthly',1014, 'Kg', 803,'Litre' ,247, 'kWh');
+INSERT INTO public.fuelconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, gasoline_consumed, gasoline_consumed_units, diesel_consumed, diesel_consumed_units, electric_consumed, electric_consumed_units)
+	VALUES (5, '2023-09-30',' 2023-10-10', 'Monthly', 1047,'kg', 866, 'Litre', 234,'kWh' );
+INSERT INTO public.fuelconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, gasoline_consumed, gasoline_consumed_units, diesel_consumed, diesel_consumed_units, electric_consumed, electric_consumed_units)
+	VALUES (5, '2023-10-31', '2023-11-04', 'Monthly', 1003, 'Kg', 795,'Litre' , 220,'kWh' );
+
+SELECT * FROM FUELCONSUMPTION;
---
+++ Database/Insert_Water_Consumption.sql
@@ -0,0 +1,62 @@
+INSERT INTO public.waterconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, drinking_water_consumption, drinking_water_consumption_units, washroom_water_consumption, washroom_water_consumption_unit, other_purpose_consumption, other_purpose_consumption_unit)
+	VALUES (1, '2023-08-31', '2023-09-13','Monthly',5257, 'LITRE', 8943, 'LITRE', 425, 'LITRE');
+	
+INSERT INTO public.waterconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, drinking_water_consumption, drinking_water_consumption_units, washroom_water_consumption, washroom_water_consumption_unit, other_purpose_consumption, other_purpose_consumption_unit)
+	VALUES (1, '2023-09-30' , ' 2023-10-18','Monthly',5298, 'LITRE', 8902, 'LITRE', 435, 'LITRE');
+	
+INSERT INTO public.waterconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, drinking_water_consumption, drinking_water_consumption_units, washroom_water_consumption, washroom_water_consumption_unit, other_purpose_consumption, other_purpose_consumption_unit)
+	VALUES (1, '2023-10-31', '2023-11-25','Monthly',5206, 'LITRE', 8985, 'LITRE', 467, 'LITRE');
+	
+INSERT INTO public.waterconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, drinking_water_consumption, drinking_water_consumption_units, washroom_water_consumption, washroom_water_consumption_unit, other_purpose_consumption, other_purpose_consumption_unit)
+	VALUES (2, '2023-08-31', '2023-09-15','Monthly',5459, 'LITRE', 9056, 'LITRE', 555, 'LITRE');
+	
+INSERT INTO public.waterconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, drinking_water_consumption, drinking_water_consumption_units, washroom_water_consumption, washroom_water_consumption_unit, other_purpose_consumption, other_purpose_consumption_unit)
+	VALUES (2, '2023-09-30' , ' 2023-10-11 ','Monthly',5411, 'LITRE', 9043, 'LITRE', 528, 'LITRE');
+	
+INSERT INTO public.waterconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, drinking_water_consumption, drinking_water_consumption_units, washroom_water_consumption, washroom_water_consumption_unit, other_purpose_consumption, other_purpose_consumption_unit)
+	VALUES (2, '2023-08-31', '2023-09-25','Monthly',5435, 'LITRE', 9089, 'LITRE', 549, 'LITRE');
+	
+INSERT INTO public.waterconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, drinking_water_consumption, drinking_water_consumption_units, washroom_water_consumption, washroom_water_consumption_unit, other_purpose_consumption, other_purpose_consumption_unit)
+	VALUES (3, '2023-08-31', '2023-09-01','Monthly',5140, 'LITRE',7893,'LITRE', 331, 'LITRE');
+
+INSERT INTO public.waterconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, drinking_water_consumption, drinking_water_consumption_units, washroom_water_consumption, washroom_water_consumption_unit, other_purpose_consumption, other_purpose_consumption_unit)
+	VALUES (3, '2023-09-30' , ' 2023-10-30','Monthly',5102, 'LITRE', 7908, 'LITRE', 378, 'LITRE');
+	
+	
+INSERT INTO public.waterconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, drinking_water_consumption, drinking_water_consumption_units, washroom_water_consumption, washroom_water_consumption_unit, other_purpose_consumption, other_purpose_consumption_unit)
+	VALUES (3, '2023-10-31', '2023-11-22','Monthly',5157, 'LITRE', 7943, 'LITRE', 305, 'LITRE');
+	
+	INSERT INTO public.waterconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, drinking_water_consumption, drinking_water_consumption_units, washroom_water_consumption, washroom_water_consumption_unit, other_purpose_consumption, other_purpose_consumption_unit)
+	VALUES (4, '2023-08-31', '2023-09-13','Monthly',5015, 'LITRE', 8083, 'LITRE', 414, 'LITRE');
+
+INSERT INTO public.waterconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, drinking_water_consumption, drinking_water_consumption_units, washroom_water_consumption, washroom_water_consumption_unit, other_purpose_consumption, other_purpose_consumption_unit)
+	VALUES (4, '2023-09-30', '2023-10-17','Monthly',5069, 'LITRE', 8003, 'LITRE', 485, 'LITRE');
+
+INSERT INTO public.waterconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, drinking_water_consumption, drinking_water_consumption_units, washroom_water_consumption, washroom_water_consumption_unit, other_purpose_consumption, other_purpose_consumption_unit)
+	VALUES (4, '2023-10-31', '2023-11-06','Monthly',5099, 'LITRE', 8047, 'LITRE', 445, 'LITRE');
+
+INSERT INTO public.waterconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, drinking_water_consumption, drinking_water_consumption_units, washroom_water_consumption, washroom_water_consumption_unit, other_purpose_consumption, other_purpose_consumption_unit)
+	VALUES (5, '2023-08-31', '2023-09-20','Monthly',5780, 'LITRE', 9396 ,'LITRE', 654, 'LITRE');
+
+INSERT INTO public.waterconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, drinking_water_consumption, drinking_water_consumption_units, washroom_water_consumption, washroom_water_consumption_unit, other_purpose_consumption, other_purpose_consumption_unit)
+	VALUES (5, '2023-09-30' , ' 2023-10-15','Monthly',5752, 'LITRE', 9361, 'LITRE', 609, 'LITRE');
+
+INSERT INTO public.waterconsumption(
+	retail_store_id, date_generated, date_entered, data_load_type, drinking_water_consumption, drinking_water_consumption_units, washroom_water_consumption, washroom_water_consumption_unit, other_purpose_consumption, other_purpose_consumption_unit)
+	VALUES (5, '2023-10-31' ,  '2023-11-18','Monthly',5721, 'LITRE', 9358, 'LITRE', 625, 'LITRE');
+
+SELECT * FROM waterconsumption;
